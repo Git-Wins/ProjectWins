c41fb114026aa40ca8b6e57d1a5f210f4000be8b
Winston.vijay <Winston.vijay@GSR-Lap31.GSRCHENNAI.LOCAL>
2018-03-15 18:00:40 +0530

    just a minor addition to GitWork.java

--- GitWork/src/gitPushTRY/GitWork.java
+++ GitWork/src/gitPushTRY/GitWork.java
@@ -1,10 +1,10 @@
 package gitPushTRY;
 
-public class GitWork {
-
-	public static void main(String[] args) {
+public class GitWork 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("First Git-Commit and Push");
-
+		System.out.println("New change added");
 	}
-
 }
